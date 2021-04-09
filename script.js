c8cd83cf12c01a3484911b02f64d5de0f439ec49
Jakub Cebula <kubcor1@gmail.com>
2021-04-09 12:12:52 +0200

    kolokwium

---
+++ script.js
@@ -0,0 +1,7 @@
+let el = document.querySelector('.box');
+let button = document.querySelector('.button');
+
+button.addEventListener('click', function() {
+	el.style.backgroundColor = "yellow";
+});
+
---
+++ script2.js
@@ -0,0 +1,6 @@
+let przycisk = document.querySelector('.button2');
+
+przycisk.addEventListener('click', function() {
+    document.body.style.backgroundColor = "red";
+});
+
